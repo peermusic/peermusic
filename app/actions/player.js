a4e2524ac81c8de4f1cd91868ddf343fbb228fb4
queicherius <queicherius@gmx.de>
2015-12-29 04:09:04 +0100

    Add ability to "unshuffle"

--- app/actions/player.js
+++ app/actions/player.js
@@ -187,7 +187,7 @@ var actions = {
       // If we are on infinite playback, make sure that we have songs added to the automatic queue
       const automaticQueue = state.player.automaticQueue
       if (state.player.repeatPlayback && automaticQueue.length <= 15) {
-        const index = !state.player.randomPlayback ? 0 : Math.floor(Math.random() * state.player.possibleQueue.length);
+        const index = !state.player.randomPlayback ? 0 : Math.floor(Math.random() * state.player.possibleQueue.length)
         actions.PLAYBACK_SONG(state.player.possibleQueue, index, true)(dispatch, getState)
       }
 
@@ -215,6 +215,12 @@ var actions = {
 
       if (!state.player.randomPlayback) {
         dispatch({type: 'SHUFFLE_AUTOMATIC_QUEUE'})
+      } else {
+        const index = state.player.possibleQueue.indexOf(state.player.songId)
+        dispatch({
+          type: 'PLAYBACK_AUTOMATIC_QUEUE',
+          songs: [...state.player.possibleQueue.slice(index + 1)]
+        })
       }
 
       dispatch({type: 'TOGGLE_RANDOM_PLAYBACK'})
