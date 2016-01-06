053b0781f0fb9a27e1a559eee1688734dabd2f82
Per Guth <mail@perguth.de>
2016-01-06 12:30:13 +0100

    reintroduced friends actions for abstraction

--- app/actions/instances.js
+++ app/actions/instances.js
@@ -43,25 +43,7 @@ var actions = {
 
   RECEIVE_INVITE: () => {
     console.log(swarm)
-  },
-
-  // Add a new scraping server
-  ADD_FRIEND: (description, friendUrl) => {
-    return {
-      type: 'ADD_FRIEND',
-      description,
-      friendUrl
-    }
-  },
-
-  // Remove a scraping server
-  REMOVE_FRIEND: (index) => {
-    return {
-      type: 'REMOVE_FRIEND',
-      index
-    }
   }
-
 }
 
 module.exports = actions
