332e6728e56a731b4046bcc6b5754855d0d6a6e2
queicherius <queicherius@gmx.de>
2016-01-24 23:11:52 +0100

    Render QR codes for invite links

--- app/components/ManageFriends/IssuedInvitesTable.jsx
+++ app/components/ManageFriends/IssuedInvitesTable.jsx
@@ -3,6 +3,7 @@ const { connect } = require('react-redux')
 const classNames = require('classnames')
 const CopyableInput = require('../CopyableInput.jsx')
 const { DISCARD_ISSUED_INVITE } = require('../../actions')
+const QRButton = require('../QRButton.jsx')
 
 function IssuedInvitesTable ({ issuedInvitesList, DISCARD_ISSUED_INVITE }) {
   return (
@@ -12,6 +13,7 @@ function IssuedInvitesTable ({ issuedInvitesList, DISCARD_ISSUED_INVITE }) {
           <th className='number'>#</th>
           <th>Description</th>
           <th>Invite URI</th>
+          <th className='qr desktop-only'>Invite QR-Code</th>
           <th className='remove-button'/>
         </tr>
         {issuedInvitesList.map((invite, i) => {
@@ -24,6 +26,10 @@ function IssuedInvitesTable ({ issuedInvitesList, DISCARD_ISSUED_INVITE }) {
               <td className={inviteClass}>
                 <span className='mobile-column-heading mobile-only'>{invite.description || 'No description'}</span>
                 <CopyableInput value={invite.uri || '—'}/>
+                <div className='mobile-qr-button mobile-only'><QRButton data={invite.uri}/></div>
+              </td>
+              <td className='desktop-only'>
+                <QRButton data={invite.uri}/>
               </td>
               <td className='remove-button'><a onClick={() => DISCARD_ISSUED_INVITE(invite.sharedSignPubKey)}><i className='fa fa-trash'/></a></td>
             </tr>
