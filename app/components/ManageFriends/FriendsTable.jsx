99513079420f56e2c236cb1c8700a28bd1b649d7
queicherius <queicherius@gmx.de>
2016-01-25 20:18:02 +0100

    UI to display device/friend connection state

--- app/components/ManageFriends/FriendsTable.jsx
+++ app/components/ManageFriends/FriendsTable.jsx
@@ -10,6 +10,7 @@ function FriendsTable ({ friends, REMOVE_PEER }) {
         <tbody>
         <tr>
           <th className='number'>#</th>
+          <th className='status desktop-only'>Status</th>
           <th>Description</th>
           <th>Friend ID</th>
           <th className='remove-button'/>
@@ -19,10 +20,17 @@ function FriendsTable ({ friends, REMOVE_PEER }) {
           return (
               <tr key={i}>
                 <td className='number'>{i + 1}</td>
+                <td className='status desktop-only'>
+                  {friend.online ? <span className='online'><i className='fa fa-circle'/> Online</span> : <span className='offline'><i className='fa fa-circle-o'/> Offline</span>}
+                </td>
                 <td className={descriptionClass}>{friend.description || '—'}</td>
                 <td className='break-cell'>
                   <span className='mobile-column-heading mobile-only'>{friend.description || 'No description'}</span>
                   <CopyableInput value={friend.peerId}/>
+                  <div className='mobile-only mobile-status'>
+                    Status:
+                    {friend.online ? <span className='online'><i className='fa fa-circle'/> Online</span> : <span className='offline'><i className='fa fa-circle-o'/> Offline</span>}
+                  </div>
                 </td>
                 <td className='remove-button'><a onClick={() => REMOVE_PEER(friend.peerId)}><i className='fa fa-trash'/></a></td>
               </tr>
